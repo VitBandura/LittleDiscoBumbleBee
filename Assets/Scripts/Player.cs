2702df5bd73cfcc29fc5461c0cadd7590c017c3b
Vit <banduravit@gmail.com>
2022-03-03 22:41:37 +0300

    Added player movement script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,36 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+   [SerializeField] private float _movementSpeed;
+   [SerializeField] private float _verticalOffset;
+   [SerializeField] private float _maxHeight;
+   [SerializeField] private float _minHeight;
+   
+   private Vector3 _targetPosition;
+   private void Start()
+   {
+      _targetPosition = new Vector3(-20,0);
+   }
+
+   private void Update()
+   {
+      var step = _movementSpeed * Time.deltaTime;
+      transform.position = Vector3.MoveTowards(transform.position, _targetPosition, step);
+      Fly();
+   }
+
+   private void Fly()
+   {
+      if (Input.GetKeyDown(KeyCode.W) && _targetPosition.y < _maxHeight)
+      {
+         _targetPosition = new Vector3(_targetPosition.x, _targetPosition.y + _verticalOffset);
+      }
+      else if (Input.GetKeyDown(KeyCode.S) && _targetPosition.y > _minHeight)
+      {
+         _targetPosition = new Vector3(_targetPosition.x, _targetPosition.y - _verticalOffset);
+      }
+   }
    
 }
