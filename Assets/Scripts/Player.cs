ded8abbd0009d41cca3f0c992d9ccb6ecf756963
Vit <banduravit@gmail.com>
2022-03-12 17:16:01 +0300

    Added simple score counter and background. Small speed changes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Globalization;
 using UnityEngine;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
@@ -6,7 +9,10 @@ public class Player : MonoBehaviour
    [SerializeField] private float _verticalOffset;
    [SerializeField] private float _maxHeight;
    [SerializeField] private float _minHeight;
-   
+
+   [SerializeField] private TMP_Text _scoreUI;
+
+   private float _score;
    private Vector3 _targetPosition;
    private readonly Vector3 _startPosition = new(-20, 0);
    
@@ -14,6 +20,8 @@ public class Player : MonoBehaviour
    {
       transform.position = _startPosition;
       _targetPosition = _startPosition;
+      
+     _scoreUI.text = _score.ToString();
    }
    private void Update()
    {
@@ -30,6 +38,19 @@ public class Player : MonoBehaviour
       else if (Input.GetKeyDown(KeyCode.S) && _targetPosition.y > _minHeight)
       {
          _targetPosition = new Vector3(_targetPosition.x, _targetPosition.y - _verticalOffset);
+      }
+   }
+
+   /*
+    * create separated class about handling collisions
+    */
+   private void OnTriggerEnter2D(Collider2D other)
+   {
+      if (other.gameObject.CompareTag("Flower"))
+      {
+         Destroy(other.gameObject);
+         _score++;
+        _scoreUI.text = _score.ToString();
       }
    }
 }
